cd9924510c50c8ee312b76eabe43bb656b36bf4b
kumpmati <kumpulainen.matias@gmail.com>
2018-11-11 18:25:30 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    consumer_key:         'Iz95ekBxdhdAYTX4uZ6pHFMqg',
-    consumer_secret:      'fNSShNZ0mlqjLVmXIwiwJjWaNRxRydcnGdJFXd8KXc77JkB5Gv',
-    access_token:         '825302034639364096-5KanJ4GGmBbvS69czo3UGACkTmBHNBj',
-    access_token_secret:  'NMkLWuwnSYoTwwspKpgepUwuolFcsxY3OzyNajmwfU4fD',
+    consumer_key:         'CONSUMER_KEY',
+    consumer_secret:      'CONSUMER_SECRET',
+    access_token:         'ACCESS_TOKEN',
+    access_token_secret:  'ACCESS_TOKEN_SECRET',
     timeout_ms:           60*1000  // optional HTTP request timeout to apply to all requests. 
 };
